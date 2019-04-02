f16a0f9aac4a1cca8f7caf29a37ae94ae93ba267
Fuzail Ahmed <fuzail1280@gmail.com>
2019-04-02 08:21:33 +0530

    1. Added csurf package (`npm i` needed) 2. Added csuf in auth routes for xss protection 3. Successfully integrated token in the login and signup views

--- routes/auth.js
+++ routes/auth.js
@@ -1,25 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var csrf = require('csurf')
+var bodyParser = require('body-parser')
+
+// setup csrf route middlewares
+var csrfProtection = csrf({ cookie: true })
+var parseForm = bodyParser.urlencoded({ extended: false })
+
+
 
 /* GET users listing. */
-router.get('/login', function(req, res, next) {
-    res.render('auth/login', { title: 'Plainsurf | Login', layout: '/layouts/auth_layouts' });
+router.get('/login', csrfProtection, function (req, res, next) {
+    res.render('auth/login', { title: 'Plainsurf | Login', csrfToken: req.csrfToken(), layout: '/layouts/auth_layouts' });
 });
 
 
 /* GET users listing. */
-router.get('/signup', function(req, res, next) {
-    res.render('auth/signup', { title: 'Plainsurf | Sign up', layout: '/layouts/auth_layouts' });
+router.get('/signup', csrfProtection, function (req, res, next) {
+    res.render('auth/signup', { title: 'Plainsurf | Sign up', csrfToken: req.csrfToken(), layout: '/layouts/auth_layouts' });
 });
 
-router.post('/login', function(req, res, next) {
-    console.log(req.body)
-     res.json(req.body);
+router.post('/login', parseForm, csrfProtection, function (req, res, next) {
+    try {
+        return res.json(req.body);
+    } catch (error) {
+        res.json({ "message": error.message });
+    }
+
 });
 
-router.post('/signup', function(req, res, next) {
-    console.log(req.body)
-     res.json(req.body);
+router.post('/signup', parseForm, csrfProtection, function (req, res, next) {
+    try {
+        return res.json(req.body);
+    } catch (error) {
+        res.json({ "message": error.message });
+    }
 });
 
 module.exports = router;
